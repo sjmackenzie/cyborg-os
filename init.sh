ea54fdd837583949bd5c1cf5d246e42e6082544e
Stewart Mackenzie <setori88@gmail.com>
2012-11-07 02:58:20 +0800

    reworked the module system

--- init.sh
+++ init.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-args=("$@")
-username=${args[0]}
-
-if [ -z ${username} ]; then
-    git submodule add -f git://github.com/cyborgos/cyborg-vm.git vm
-    git submodule add -f git://github.com/cyborgos/cyborg-bootcompiler.git bootcompiler
-    git submodule add -f git://github.com/cyborgos/cyborg-library.git lib/main
-    git submodule add -f git://github.com/cyborgos/cyborg-compiler.git lib/compiler
-else
-    git submodule add -f git@github.com:${username}/cyborg-vm.git vm
-    git submodule add -f git@github.com:${username}/cyborg-bootcompiler.git bootcompiler
-    git submodule add -f git@github.com:${username}/cyborg-library.git lib/main
-    git submodule add -f git@github.com:${username}/cyborg-compiler.git lib/compiler
-fi
+git submodule init
+git submodule update
